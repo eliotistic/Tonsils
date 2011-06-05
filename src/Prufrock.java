48964a4bd28d78ae2e63faa11864ff49df5e8c49
Eliot Handelman <eliot@colba.net>
2011-06-05 14:25:10 -0400

    foo

--- src/Prufrock.java
+++ src/Prufrock.java
@@ -3,6 +3,7 @@ public class Prufrock {
 
 		public static void main(String[] args) {
 			System.out.println("Let us git then you and I.");
+			System.out.println("When the evening is spread out.");
 		}
 	}
 
